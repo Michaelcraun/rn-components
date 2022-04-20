457cce2b5d1d8634e10a742c8b64d1ab8956b684
Michael Craun <michael.craun@gmail.com>
2022-04-20 16:53:02 -0400

    initial commit

---
+++ function.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// Spacing on closure is stupidly important!!
+const Avatar = () => {
+    return <View style={{height: 50, width: 50, borderRadius: 25, backgroundColor: 'green'}}></View>
+};
+
+export default Avatar;
