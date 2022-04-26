9d4ecedcd80b880cf504e84e349b55d03d81ac11
Michael Craun <michael.craun@gmail.com>
2022-04-26 07:28:14 -0400

    Added notes

--- function.js
+++ function.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { View } from 'react-native';
 
 // Spacing on closure is stupidly important!!
+// Named export `export const Avatar` must be imported via 
+// `import { Avatar } from './function';` but allows you to export multiple
+// objects from the same file.
 const Avatar = () => {
     return <View style={{height: 50, width: 50, borderRadius: 25, backgroundColor: 'green'}}></View>
 };
