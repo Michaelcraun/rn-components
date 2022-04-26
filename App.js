c88899d86820f9f671ccf9a206e346e3a43e551c
Michael Craun <michael.craun@gmail.com>
2022-04-26 08:33:54 -0400

    Lesson 23: JS Objects

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Avatar from './function';
-import Button from './button-func';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Avatar from "./function";
+import Button from "./button-func";
+import { TouchableOpacity } from "react-native";
+
+function printHello(person) {
+  console.log(
+    `hello, ${person.name}! You are a ${person.age} year old ${person.gender}.`
+  );
+}
 
 export default function App() {
   let price = 45;
-  let discount = price > 40 ? '20%' : '10%'
+  let discount = price > 40 ? "20%" : "10%";
+  let name = "Michael";
+
+  const person = {
+    name: "John",
+    age: 34,
+    gender: "male",
+  };
+  const person2 = { ...person, age: 21 };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Avatar></Avatar>
-      <Text>What is 1+1? {1+1}</Text>
-      <Text>The discount is: { discount }</Text>
+      <Text>What is 1+1? {1 + 1}</Text>
+      <Text>The discount is: {discount}</Text>
       <Text>Logical operators: && ||</Text>
-      <Text>The deal is { discount === '20%' && price < 50 ? "hot!" : "nah..." }</Text>
+      <Text>
+        The deal is {discount === "20%" && price < 50 ? "hot!" : "nah..."}
+      </Text>
 
-      
-      <Button title='Click Me!' style={{ marginBottom: 20 }} outlined={true}/>
-      <Button title='Dont Click Me!'/>
-      <Button title='Title Button!' />
+      <Button title="Click Me!" style={{ marginBottom: 20 }} outlined={true} />
+      <Button title="Dont Click Me!" />
+      <Button title="Title Button!" />
+
+      <TouchableOpacity
+        onPress={() => printHello(person2)}
+        constructor={() => {
+          this.onPress = this.onPress.bind(this);
+        }}
+      >
+        <Text>Click Me!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -26,9 +52,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
--- function.js
+++ function.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 // Spacing on closure is stupidly important!!
-// Named export `export const Avatar` must be imported via 
+// Named export `export const Avatar` must be imported via
 // `import { Avatar } from './function';` but allows you to export multiple
 // objects from the same file.
 const Avatar = () => {
-    let price = '$50';
-    return <View style={{height: 50, width: 50, borderRadius: 25, backgroundColor: 'green', alignItems: 'center', justifyContent: 'center'}}>
-        <Text>{price}</Text>
+  let price = "$50";
+  return (
+    <View
+      style={{
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        backgroundColor: "green",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>{price}</Text>
     </View>
+  );
 };
 
-export default Avatar; 
+export default Avatar;
